9e3f64705483929a8b51c10801b938bbfbcc3dbd
jambumoses <jambcareer@gmail.com>
2026-05-12 10:20:09 -0700

    updates

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
-  basePath: '/portfolio',
-  assetPrefix: '/portfolio/',
-  images: {
-    unoptimized: true,
-  },
+  // basePath: '/portfolio',
+  // assetPrefix: '/portfolio/',
+  // images: {
+  //   unoptimized: true,
+  // },
 };
 
 export default nextConfig;

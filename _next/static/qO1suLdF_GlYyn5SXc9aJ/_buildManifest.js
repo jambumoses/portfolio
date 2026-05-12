9e552636b0e24a8bee0ec4e0f31ba3eb855bcdbe
jambumoses <jambcareer@gmail.com>
2026-05-12 10:01:03 -0700

    Updates

--- _next/static/qO1suLdF_GlYyn5SXc9aJ/_buildManifest.js
+++ _next/static/qO1suLdF_GlYyn5SXc9aJ/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/portfolio//_next/:path+",
+        "destination": "/portfolio/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [

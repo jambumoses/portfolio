8231613c2e7c600526ff57641cb9a5b1b5344cf8
jambumoses <jambcareer@gmail.com>
2026-05-12 09:59:42 -0700

    base url for github export deployment

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-   output: 'export',
+  output: 'export',
+  basePath: '/portfolio',
+  assetPrefix: '/portfolio/',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
